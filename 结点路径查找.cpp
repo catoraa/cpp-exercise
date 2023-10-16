ac948ef654e8b3c9ef30805534162b82bccc5a6e
catoraa <1164726499@qq.com>
2023-10-16 13:43:16 +0800

    -

--- 结点路径查找.cpp
+++ 结点路径查找.cpp
@@ -1,1 +1,27 @@
 #include<stdio.h>
+
+struct TreeNode
+{
+    int data;
+    TreeNode *left,*right;
+};
+
+int findtrack(TreeNode *T,TreeNode *N){
+    if (!T)return 0;
+    
+    else if (T==N){
+        printf("%d",T->data);
+        return 1;
+    }
+
+    else if (findtrack(T->left,N)||findtrack(T->right,N)){
+        printf("%d",T->data);
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(){
+    return 0;
+}
