08e2431edaa6af50243f135945f78233bddd326d
catoraa <1164726499@qq.com>
2023-11-08 16:27:22 +0800

    -

---
+++ 408-数据结构经典算法/=kmp算法.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+// 计算模式串b的next数组
+void getNext(string b, int next[]) {
+
+}
+
+// KMP算法匹配
+void kmp(string a, string b) {
+    
+}
+
+int main() {
+    string a = "abaabaabcabaabc";
+    string b = "abaabc";
+    kmp(a, b);
+    return 0;
+}
