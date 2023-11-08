30ed30d522758516b659a776088c7e95fbfd2d45
catoraa <1164726499@qq.com>
2023-11-08 16:30:13 +0800

    -

--- 408-真题/2013-2.cpp
+++ 408-真题/2013-2.cpp
@@ -3,13 +3,13 @@
 //摩尔投票法
 //简单
 
-int findmain(int *a，int len){
+int findmain(int *a,int len){
     int main=a[0],count=1,sum=0;
     for (int i = 1; i < len; i++)
     {   
-        if (a[i]==main)count++,sum++;//元素相同，“加强”
-        else count--;//元素不同，“对冲”
-        if(count==0)main=a[i],sum=0;//换元素，个数计数归零
+        if (a[i]==main)count++,sum++;//元素相同，“提升支持率”
+        else count--;//元素不同，“降低支持率”
+        if(count==0)main=a[i],sum=0;//支持率归零，更换元素，个数计数归零
     }
     if(sum>len/2)return main;
     else return -1;
