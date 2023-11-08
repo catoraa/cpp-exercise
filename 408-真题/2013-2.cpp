ed37416feed21eb296e2230317c7eaa0a41696de
catoraa <1164726499@qq.com>
2023-11-09 02:10:55 +0800

    -

--- 408-真题/2013-2.cpp
+++ 408-真题/2013-2.cpp
@@ -11,10 +11,9 @@ int findmain(int *a,int len){
         else count--;//元素不同，“降低支持率”
         if(count==0)main=a[i],sum=0;//支持率归零，更换元素，个数计数归零
     }
-    if(sum>len/2)return main;
-    else return -1;
+    if(sum>len/2)return main;//判定支持率最高元素符不符合主元素条件
+    else return -1;//找不到主元素
 }
-
 int main(){
 
     return 0;
