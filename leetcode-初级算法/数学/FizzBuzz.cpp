bd531decc896a30dc75df29a148dfb0b76a699c9
catoraa <1164726499@qq.com>
2023-11-09 11:07:23 +0800

    -

---
+++ leetcode-初级算法/数学/FizzBuzz.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> f;
+        string a="FizzBuzz";
+        string b="Fizz";
+        string c="Buzz";
+        for (int i = 1; i <= n; i++)
+        {   
+            int mod3=i%3,mod5=i%5;//减少计算量
+            if(mod3==0&&mod5==0)f.push_back(a);
+            else if(mod3==0)f.push_back(b);
+            else if(mod5==0)f.push_back(c);
+            else f.push_back(to_string(i));
+        }
+        return f;
+    }
+};
+
+int main(){
+
+    return 0;
+}
